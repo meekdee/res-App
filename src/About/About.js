2afbc93f21f6d61a4d32ecae7a6e755e20d99404
Daniel <drewaju.16@gmail.com>
2024-01-29 17:03:34 +0100

    dee

--- src/About/About.js
+++ src/About/About.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './About.css';
+import food from '../image/restauranfood.jpg'
 
 
 function About() {
   return (
     <div className="about">
-      <section>
+      <section style={{ float: 'left', width: '50%' }}>
         <h1>Little Lemon</h1>
         <p>Abuja</p>
         <p></p>
       </section>
-      <section>
-        <img src={Logo} height="70px" width="150px" alt="Logo of Little lemon"/>
+      <section className="foodPic">
+        <img src={food} height="200px" width="200px" alt="Logo of Little lemon"/>
       </section>
     </div>
   );
