e6fda3b293a45cab76717adbefae196fa8952ad7
Daniel <drewaju.16@gmail.com>
2024-01-29 16:38:48 +0100

    dee

--- src/About/About.js
+++ src/About/About.js
@@ -5,7 +5,11 @@ import './About.css';
 function About() {
   return (
     <div className="about">
-       <h1>Little Lemon</h1>
+      <section>
+        <h1>Little Lemon</h1>
+        <p>Abuja</p>
+        <p></p>
+      </section>
     </div>
   );
 }
