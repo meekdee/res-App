179ff2367cb9aecf9343f2c28b054b88a3c83c3c
Daniel <drewaju.16@gmail.com>
2024-01-29 17:20:42 +0100

    dee

--- src/About/About.js
+++ src/About/About.js
@@ -9,11 +9,16 @@ function About() {
       <section style={{ float: 'left', width: '50%' }}>
         <h1>Little Lemon</h1>
         <p>Abuja</p>
-        <p></p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
       </section>
       <section className="foodPic">
-        <img src={food} height="200px" width="200px" alt="Logo of Little lemon"/>
+        <img src={food} height="300px" width="250px" alt="Logo of Little lemon"/>
       </section>
+      <div className="button">
+        
+      </div>
     </div>
   );
 }
